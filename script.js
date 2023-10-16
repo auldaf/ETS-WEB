affcbf8ca703bde6cd696d1341c9c9e52b4a076a
auldaf <auliadaffarahmani@gmail.com>
2023-10-16 09:33:30 +0700

    ETS

--- script.js
+++ script.js
@@ -8,9 +8,9 @@ const nzd_usd = document.getElementById('nzd-usd');
 const usd_cad = document.getElementById('usd-cad');
 
 async function konvert(mataUang){
-    const url = `https://api.currencylayer.com/live? access_key = 5fd45476dbe5b2fefe897bcf03a66fd7`;
-    const uang_data = await fetch(`${url}`). then(response => response.json());
-
+    // const url = `http://api.currencylayer.com/live?access_key=5fd45476dbe5b2fefe897bcf03a66fd7&currencies=${mataUang}&format=1`;
+    const url = `http://apilayer.net/api/live? access_key = 5fd45476dbe5b2fefe897bcf03a66fd7& currencies = EUR,GBP,CAD,PLN& source = USD& format = 1`;
+    const uang_data = await fetch(`${url}`).then(response => response.json());
     console.log("run");
     console.log(uang_data);
 }
