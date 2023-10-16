cbe1d6c4da262bcdbf3e5c7c06088d7c2bf95752
auldaf <auliadaffarahmani@gmail.com>
2023-10-16 09:16:54 +0700

    ETS

---
+++ script.js
@@ -0,0 +1,18 @@
+const euro_usd = document.getElementById('euro-usd');
+const euro_gbp = document.getElementById('euro-gbp');
+const gbp_usd = document.getElementById('gbp-usd');
+const usd_jpy = document.getElementById('usd-jpy');
+const aud_usd = document.getElementById('aud-usd');
+const usd_chf = document.getElementById('usd-chf');
+const nzd_usd = document.getElementById('nzd-usd');
+const usd_cad = document.getElementById('usd-cad');
+
+async function konvert(mataUang){
+    const url = `https://api.currencylayer.com/live? access_key = 5fd45476dbe5b2fefe897bcf03a66fd7`;
+    const uang_data = await fetch(`${url}`). then(response => response.json());
+
+    console.log("run");
+    console.log(uang_data);
+}
+
+konvert('USDEUR');
